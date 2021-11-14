f7ff2010f48242249d1fa7d7b42acc8659c27956
M.T.C Lakshitha <chameera@encyteus.com>
2021-11-14 13:04:31 +0530

    change mongo db host

--- note-service/server/data-access/index.js
+++ note-service/server/data-access/index.js
@@ -12,9 +12,10 @@ const mongoUserName = MONGO_USERNAME || "reservation"
 const mongoPassword = MONGO_PASSWORD || "g33xegQqJcGTdRyu"
 const mongoPort = MONGO_PORT || 27017
 const mongoDb = MONGO_DB || "reservation"
-const mongoHost = MONGO_HOST || "18.218.196.254"
+const mongoHost = MONGO_HOST || "3.144.191.91"
+const mongoPrivateHost = PRIVATE_IP || "172.31.39.44"
 
-const client = new MongoClient(new Server('18.218.196.254', 27017), { useUnifiedTopology: true, useNewUrlParser: true })
+const client = new MongoClient(new Server(mongoHost, 27017), { useUnifiedTopology: true, useNewUrlParser: true })
 export async function makeDb () {
   if (!client.isConnected()) {
     await client.connect()
